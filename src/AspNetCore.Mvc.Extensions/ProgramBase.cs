8bb62185d89e3184ae9d1d9dbce2fead27767b4c
David Ikin <davidikin45@gmail.com>
2020-04-09 17:44:57 +0100

    changes

--- src/AspNetCore.Mvc.Extensions/ProgramBase.cs
+++ src/AspNetCore.Mvc.Extensions/ProgramBase.cs
@@ -66,7 +66,7 @@ namespace AspNetCore.Mvc.Extensions
            .UseSerilog()
            .UseAutofacMultitenant((context, options) => {
                 options.ValidateOnBuild = false;
-                options.MapDefaultTenantToAllRootDomains();
+                options.MapDefaultTenantToAllRootAndSubDomains();
                 options.AddTenantsFromConfig(context.Configuration);
                 options.ConfigureTenants(builder => {
                     builder.MapToTenantIdSubDomain();
