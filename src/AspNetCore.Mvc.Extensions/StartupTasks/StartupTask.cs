d182c82a050d5242a9fb1f6dc780b7a26bcbdf4b
David Ikin <davidikin45@gmail.com>
2020-04-10 10:59:44 +0100

    changes

--- src/AspNetCore.Mvc.Extensions/StartupTasks/StartupTask.cs
+++ src/AspNetCore.Mvc.Extensions/StartupTasks/StartupTask.cs
@@ -1,9 +1,33 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace AspNetCore.Mvc.Extensions.StartupTasks
 {
+    public abstract class ScopedStartupTaskBlocking : IStartupTask
+    {
+        private readonly IServiceProvider _serviceProvider;
+        public ScopedStartupTaskBlocking(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public abstract int Order { get; }
+
+        protected abstract Task ExecuteAsync(IServiceProvider scopedServiceProvider, CancellationToken stoppingToken);
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                await ExecuteAsync(scope.ServiceProvider, cancellationToken);
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+
     public abstract class StartupTaskBlocking : IStartupTask
     {
         public abstract int Order { get; }
